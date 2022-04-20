eb910bf47253621892f0b6fc0d43a02a02c37a44
Stefan Ritter <60473875+gideonstar-git@users.noreply.github.com>
2022-04-20 22:14:04 +0200

    Init

---
+++ beer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from random import choice
+
+slogans = [
+        "Ist der Ruf erst ruiniert, säuft's sich völlig ungeniert.",
+        "Unser letzter Wille, immer mehr Promille.",
+        "Von der Wiege bis zur Bahre, ist der Suff das einzig Wahre.",
+        "Mein größter Feind, das ist der Alkohol. Doch in der Bibel steht geschrieben, du sollst auch deine Feinde lieben.",
+        "Genug getrunken, jetzt wird gesoffen!",
+        "Es trinkt der Mensch, es säuft das Pferd. Nur heute ist es umgekehrt.",
+        "Zu viel kann man nie trinken, doch trinkt man nie genug.",
+        "Es tut mir im Herz so weh, wenn ich vom Glas den Boden seh'.",
+        "Sport ist Mord, nur Sprit hält fit.",
+        "Zwischen Leber und Nierchen passt immer noch ein Bierchen.",
+        "Morgens ein Bier und der Tag gehört dir.",
+        "Bier ist lecker, Bier ist toll. Am liebsten bin ich voll.",
+        "Der Kopf tut weh, die Füße stinken. Höchste Zeit ein Bier zu trinken.",
+        "Auch Wasser wird zum edlen Tropfen, mischt man es mit Malz und Hopfen.",
+        "Müde bin ich, geh' zur Ruh', decke meinen Bierbauch zu.",
+        "Wer Hopfen sät wird Bier ernten.",
+        "Bier kaltstellen ist auch irgendwie kochen.",
+        "Zwischen Leber und Milz passt immer noch ein Pils.",
+        "Trinken ist ein Laster - aber ein schönes.",
+        "Von der Mitte zur Titte zum Sack, Zack Zack.",
+        "Ein Gläschen in Ehren kann niemand verwehren.",
+        "Wo Saufen eine Ehre ist, kann kotzen keine Schande sein.",
+        "Jeder muss an etwas glauben. Ich glaub' ich trink' noch einen.",
+        "Lieber in der dunkelsten Kneipe als am hellsten Arbeitsplatz.",
+        "Was früher meine Leber war, ist heute eine Minibar.",
+        "Trink dein Bier bevor es Alt wird.",
+        "Freibier for future.",
+        "Der kluge Mensch, so glaubt es mir, der redet nicht und trinkt sein Bier.",
+        "Mit des Bieres Hochgenuss, wächst des Bauches Radius.",
+        "Save water, drink beer.",
+        "Im Himmel gibt's kein Bier, drum trinken wir es hier.",
+        "Sei immer du selbst. Außer es gibt Bier, dann sei betrunken.",
+        "Wer das Bier nicht ehrt, ist des Deliriums nicht wert.",
+        "Wasser schmeckt erst, wenn es in der Brauerei gewesen ist.",
+        ]
+
+print(choice(slogans))
+
